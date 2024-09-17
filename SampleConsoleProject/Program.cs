10e5f7358a5726f73aa14abee8d2ceb5ee8d7c2a
Michael Ludwig <mludwig2@css.edu>
2024-09-17 11:04:00 -0500

    Testing github Changes

--- SampleConsoleProject/Program.cs
+++ SampleConsoleProject/Program.cs
@@ -3,4 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Adding a line in Visual Studio");
 // added this line -- added to comment
 Console.WriteLine("Adding a line in VS Code");
-
+Console.WriteLine("Michael's line has been added from VS Code on the Mac");
+// added this line above
